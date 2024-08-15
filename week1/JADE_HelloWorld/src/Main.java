ce58e60cc2556aff81caa8e0d62798b7a9d23af2
Trần Hải Phong <tranhaiphong2016fpt@gmail.com>
2024-08-15 21:02:01 +1000

    update for submission week 3

--- week1/JADE_HelloWorld/src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import jade.core.Agent;
-
-public class Main extends Agent {
-    public static void main(String[] args) {
-
-    }
-}
